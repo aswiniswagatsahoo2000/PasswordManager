22cc83d08e9e8bb8834a5bddf89d18c60855974a
aswiniswagatsahoo2000 <132925978+aswiniswagatsahoo2000@users.noreply.github.com>
2023-11-18 23:00:36 +0530

    Add files via upload

---
+++ Password-Manager WebSite/script.js
@@ -0,0 +1,104 @@
+function maskPassword(pass) {
+  let str = "";
+  for (let index = 0; index < pass.length; index++) {
+    str += "*";
+  }
+  return str;
+}
+
+function copyText(txt) {
+  navigator.clipboard.writeText(txt).then(
+    () => {
+      document.getElementById("alert").style.display = "inline";
+      setTimeout(() => {
+        document.getElementById("alert").style.display = "none";
+      }, 2000);
+    },
+    () => {
+      alert("Clipboard copied Failed ");
+    }
+  );
+}
+
+const deletePassword = (website) => {
+  let data = localStorage.getItem("passwords");
+  let arr = JSON.parse(data);
+  arrUpdated = arr.filter((e) => {
+    return e.website != website;
+  });
+  localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+  alert(`Successfully deleted ${website}'s password `);
+  showPasswords();
+};
+
+//Logic to fill the table:
+
+const showPasswords = () => {
+  let tb = document.querySelector("table");
+  let data = localStorage.getItem("passwords");
+  if (data == null) {
+    tb.innerHTML = "No Datat to Show";
+  } else {
+    tb.innerHTML = ` <tr>
+    <th>Websites</th>
+    <th>UserName</th>
+    <th>Password</th>
+    <th>Delete</th>
+</tr>`;
+    let arr = JSON.parse(data);
+    let str = "";
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      str += `<tr>
+    <td>${element.website}<img onclick="copyText('${
+        element.website
+      }')" src="copy.svg" alt="Copy Button" width="15" height="15"></td>
+    <td>${element.username}<img onclick="copyText('${
+        element.username
+      }')" src="copy.svg" alt="Copy Button" width="15" height="15"></td>
+    <td>${maskPassword(element.password)}<img onclick="copyText('${
+        element.password
+      }')" src="copy.svg" alt="Copy Button" width="15" height="15"></td>
+    <td>
+        <button class="btnsm" onclick="deletePassword('${
+          element.website
+        }')">Delete</button>
+    </td>
+</tr>`;
+    }
+    tb.innerHTML = tb.innerHTML + str;
+  }
+  //<--------------To Reset the form--------------->
+  website.value = "";
+  username.value = "";
+  password.value = "";
+};
+console.log("Working");
+showPasswords();
+document.querySelector(".btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Clicked..");
+  console.log(username.value, password.value);
+  let passwords = localStorage.getItem("passwords");
+  console.log(passwords);
+  if (passwords == null) {
+    let json = [];
+    json.push({
+      website: website.value,
+      username: username.value,
+      password: password.value,
+    });
+    alert("password Saved");
+    localStorage.setItem("passwords", JSON.stringify(json));
+  } else {
+    let json = JSON.parse(localStorage.getItem("passwords"));
+    json.push({
+      website: website.value,
+      username: username.value,
+      password: password.value,
+    });
+    alert("password Saved");
+    localStorage.setItem("passwords", JSON.stringify(json));
+  }
+  showPasswords();
+});
